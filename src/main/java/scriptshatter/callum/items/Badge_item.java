465a1f81a81477cdac336932d9aaa48b35442241
Scriptshatter <33846202+Scriptshatter@users.noreply.github.com>
2023-10-31 11:13:26 -0400

    Upgrades
    Upgrades are seperate items cause I couldn't figure out the texture junk

--- src/main/java/scriptshatter/callum/items/Badge_item.java
+++ src/main/java/scriptshatter/callum/items/Badge_item.java
@@ -1,16 +1,25 @@
 package scriptshatter.callum.items;
 
-import net.minecraft.block.GrassBlock;
-import net.minecraft.client.color.world.GrassColors;
+import io.github.apace100.apoli.power.Power;
+import io.github.apace100.apoli.power.PowerType;
+import io.github.apace100.apoli.power.PowerTypeReference;
 import net.minecraft.item.Item;
-import net.minecraft.loot.function.LootFunctionManager;
 import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.Nullable;
 
 public class Badge_item extends Item {
     //NBT values are: valid_armor, upgrade_group, Powers
+    @Nullable
+    public final Identifier valid_armor;
+    @Nullable
+    public final String upgrade_group;
+    public final PowerType<?>[] powers;
 
-    public Badge_item(Settings settings) {
+    public Badge_item(Settings settings, @Nullable Identifier valid_armor, @Nullable String upgradeGroup, PowerType<?>[] powers) {
         super(settings);
+        this.valid_armor = valid_armor;
+        upgrade_group = upgradeGroup;
+        this.powers = powers;
     }
 
 
--- src/main/java/scriptshatter/callum/items/ItemRegister.java
+++ src/main/java/scriptshatter/callum/items/ItemRegister.java
@@ -1,24 +1,33 @@
 package scriptshatter.callum.items;
 
+import io.github.apace100.apoli.power.PowerType;
+import io.github.apace100.apoli.power.PowerTypeReference;
+import io.github.apace100.origins.Origins;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import scriptshatter.callum.Callum;
 import scriptshatter.callum.armor.Cap_item;
 
 public class ItemRegister {
-    public static final Item CALLUM_PILOT = register_item("callum_pilot", new Cap_item(new FabricItemSettings().group(ItemGroup.COMBAT)));
-    public static final Item UPGRADE = register_item("upgrade", new Badge_item(new FabricItemSettings().group(ItemGroup.COMBAT)));
-
-    public static Item register_item(String name, Item item){
-        return Registry.register(Registry.ITEM, Callum.identifier(name), item);
-    }
 
     public static void register_moditems(){
         Callum.LOGGER.info("This would be a little more complicated in 1.20");
+    }
+
+    static PowerType<?> WEIRD_SKIN;
+    static {
+        WEIRD_SKIN = new PowerTypeReference<>(Origins.identifier("arcane_skin"));
+    }
+    private static final PowerType<?>[] PLACEHOLDER_PIN_POWERS = new PowerType<?>[]{
+            WEIRD_SKIN
+    };
+
+    public static final Item CALLUM_PILOT = register_item("callum_pilot", new Cap_item(new FabricItemSettings().group(ItemGroup.COMBAT)));
+    public static final Item PLACEHOLDER_PIN = register_item("placeholder_pin", new Badge_item(new FabricItemSettings().group(ItemGroup.COMBAT), Callum.identifier("callum_pilot"), null, PLACEHOLDER_PIN_POWERS));
+
+    public static Item register_item(String name, Item item){
+        return Registry.register(Registry.ITEM, Callum.identifier(name), item);
     }
 }
